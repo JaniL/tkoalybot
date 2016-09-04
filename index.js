e1e970a2a8846f673ff9ee35fad88ff557b8b7f0
Jani Luukko <jj.luukko@gmail.com>
2016-09-04 20:58:21 +0300

    Fix eslint issues

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var request = require('request')
 var fs = require('fs')
 
 var ICS_URL = 'http://ics.tko-aly.fi/'
-var jallu_URL = 'http://jalluindeksi.xyz/price'
+var JALLU_URL = 'http://jalluindeksi.xyz/price'
 var EVENTS_FILE = 'events.json'
 
 var bot = new TelegramBot(process.env.API_TOKEN, {polling: true})
@@ -14,32 +14,32 @@ fs.readFile(EVENTS_FILE, (err, data) => {
   if (!err) {
     events = JSON.parse(data)
   }
-  setTimeout(pollEvents,1000)
+  setTimeout(pollEvents, 1000)
   setInterval(pollEvents, 15 * 60 * 1000)
 })
 
-function saveEvents(data, cb) {
+function saveEvents (data, cb) {
   fs.writeFile(EVENTS_FILE, JSON.stringify(data), cb)
 }
 
-function pollEvents() {
-  retrieveEvents(function(data) {
+function pollEvents () {
+  retrieveEvents(function (data) {
     saveEvents(data)
     events = data
   })
 }
 
-function makeHumanReadable(e) {
-  return new Date(e.start).toDateString() + ": [" + e.summary.trim() + "](" + e.url + ")"
+function makeHumanReadable (e) {
+  return new Date(e.start).toDateString() + ': [' + e.summary.trim() + '](' + e.url + ')'
 }
 
-function retrieveEvents(cb) {
-  request(ICS_URL, function(err, res) {
+function retrieveEvents (cb) {
+  request(ICS_URL, function (err, res) {
     var data = ical.parseICS(res.body)
 
-    data = Object.keys(data).map(function(k) {
+    data = Object.keys(data).map(function (k) {
       return data[k]
-    }).filter(function(e) {
+    }).filter(function (e) {
       return e.type === 'VEVENT'
     })
 
@@ -47,17 +47,17 @@ function retrieveEvents(cb) {
   })
 }
 
-bot.onText(/\/jalluindeksi$/, function(msg, match) {
+bot.onText(/\/jalluindeksi$/, function (msg, match) {
   var fromId = msg.from.id
 
-  request(jallu_URL, function(err, res) {
+  request(JALLU_URL, function (err, res) {
     bot.sendMessage(fromId, 'Päivän hinta on ' + res.body + ' euroa!')
   })
 })
 
-bot.onText(/\/events$/, function(msg, match) {
+bot.onText(/\/events$/, function (msg, match) {
   var fromId = msg.from.id
-  data = events.slice(0,3).map(makeHumanReadable)
+  var data = events.slice(0, 3).map(makeHumanReadable)
 
   var res = '*Tulevat tapahtumat:* \n'
   for (var i = 0; i < data.length; i++) {
