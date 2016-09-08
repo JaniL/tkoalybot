eddbe9330c6059ce4ed1cedc3acd9b9eb7f9b153
Jani Luukko <jj.luukko@gmail.com>
2016-09-08 13:13:16 +0300

    Announce new events when found during polling

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var fs = require('fs')
 var moment = require('moment')
 var cron = require('node-cron')
 var tkoalyevents = require('tkoalyevents')
+var R = require('ramda')
 
 var EVENTS_FILE = 'events.json'
+moment.locale('fi')
 
 if (!process.env.API_TOKEN) {
   console.error('No api token found.')
@@ -18,6 +20,7 @@ var groups = []
 fs.readFile(EVENTS_FILE, (err, data) => {
   if (!err) {
     events = JSON.parse(data)
+    console.log('read', events.length, 'events')
   }
   setTimeout(pollEvents, 1000)
   setInterval(pollEvents, 15 * 60 * 1000)
@@ -27,9 +30,18 @@ function saveEvents (data, cb) {
   fs.writeFile(EVENTS_FILE, JSON.stringify(data), cb)
 }
 
+function eventDifference (data, events) {
+  var difference = R.difference(data.map(e => e.id), events.map(e => e.id))
+  return data.filter(e => difference.includes(e.id))
+}
+
 function pollEvents () {
   retrieveEvents(function (data) {
     saveEvents(data)
+    var difference = eventDifference(data, events)
+    if (difference && difference.length > 0) {
+      newEvents(difference)
+    }
     events = data
   })
 }
@@ -38,30 +50,57 @@ function getEventURL (id) {
   return 'http://tko-aly.fi/event/' + id
 }
 
-function makeHumanReadable (e) {
-  return e.starts.toDateString() + ': [' + e.name.trim() + '](' + getEventURL(e.id) + ')'
+function makeHumanReadable (dateFormat) {
+  return function (e) {
+    return moment(e.starts).format(dateFormat) + ': [' + e.name.trim() + '](' + getEventURL(e.id) + ')'
+  }
 }
 
 function retrieveEvents (cb) {
   tkoalyevents(cb)
 }
 
+function listEvents (events, dateFormat) {
+  var res = ''
+  var data = events.map(makeHumanReadable(dateFormat))
+  for (var i = 0; i < data.length; i++) {
+    var event = data[i]
+    res += event + '\n'
+  }
+  return res
+}
+
 function todaysEvents () {
   var today = moment()
   var data = events.filter(function (e) { return moment(e.starts).isSame(today, 'day') })
-                   .map(makeHumanReadable)
   if (data) {
-    var res = '*Tänään:* \n'
-    for (var i = 0; i < data.length; i++) {
-      var event = data[i]
-      res += event + '\n'
-    }
+    data = listEvents(data, 'HH:mm')
+    var res = '*Tänään:* \n' + data
     for (var j = 0; j < groups.length; j++) {
       bot.sendMessage(groups[j], res.trim(), {
         disable_web_page_preview: true,
         parse_mode: 'Markdown'
       })
     }
+  }
+}
+
+function newEvents (events) {
+  if (!events) {
+    return
+  }
+  var res
+  if (events.length > 1) {
+    res = '*Uusia tapahtumia:* \n'
+  } else {
+    res = '*Uusi tapahtuma:* \n'
+  }
+  res += listEvents(events)
+  for (var j = 0; j < groups.length; j++) {
+    bot.sendMessage(groups[j], res.trim(), {
+      disable_web_page_preview: true,
+      parse_mode: 'Markdown'
+    })
   }
 }
 
