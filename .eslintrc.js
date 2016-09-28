5f1c87f71439ecff0db00ef7c2a3cfcd744d5b68
cubrr <jay@thecuber.org>
2016-09-28 21:31:48 +0300

    Add style preferences to eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,13 @@ module.exports = {
     "plugins": [
         "standard",
         "promise"
-    ]
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "indent": ["error", 2, {"SwitchCase": 1}],
+        "semi": ["error", "never"],
+        "quotes": ["error", "single"],
+    }
 };
