41ce5602e0fad888f674104e78fb667d3e35983d
Hugo Holmqvist <hugo.holm@live.com>
2016-11-03 18:41:36 +0200

    Add translations

---
+++ translations.js
@@ -0,0 +1,97 @@
+/*
+0	tornado
+1	tropical storm
+2	hurricane
+3	severe thunderstorms
+4	thunderstorms
+5	mixed rain and snow
+6	mixed rain and sleet
+7	mixed snow and sleet
+8	freezing drizzle
+9	drizzle
+10	freezing rain
+11	showers
+12	showers
+13	snow flurries
+14	light snow showers
+15	blowing snow
+16	snow
+17	hail
+18	sleet
+19	dust
+20	foggy
+21	haze
+22	smoky
+23	blustery
+24	windy
+25	cold
+26	cloudy
+27	mostly cloudy (night)
+28	mostly cloudy (day)
+29	partly cloudy (night)
+30	partly cloudy (day)
+31	clear (night)
+32	sunny
+33	fair (night)
+34	fair (day)
+35	mixed rain and hail
+36	hot
+37	isolated thunderstorms
+38	scattered thunderstorms
+39	scattered thunderstorms
+40	scattered showers
+41	heavy snow
+42	scattered snow showers
+43	heavy snow
+44	partly cloudy
+45	thundershowers
+46	snow showers
+47	isolated thundershowers
+*/
+module.exports = {
+  'tornado': 'tornadoja',
+  'tropical storm': 'trooppisia myrskyjä',
+  'hurricane': 'hurrikaaneja',
+  'severe thunderstorms': 'voimakkaita ukkosmyrskyjä',
+  'thunderstorms': 'ukkosmyrskyjä',
+  'mixed rain and snow': '',
+  'mixed rain and sleet': '',
+  'mixed snow and sleet': '',
+  'freezing drizzle': '',
+  'drizzle': '',
+  'freezing rain': '',
+  'showers': '',
+  'snow flurries': '',
+  'light snow showers': '',
+  'blowing snow': '',
+  'snow': '',
+  'hail': '',
+  'sleet': '',
+  'dust': '',
+  'foggy': '',
+  'haze': '',
+  'smoky': '',
+  'blustery': '',
+  'windy': '',
+  'cold': '',
+  'cloudy': '',
+  'mostly cloudy (night)': '',
+  'mostly cloudy (day)': '',
+  'partly cloudy (night)': '',
+  'partly cloudy (day)': '',
+  'clear (night)': '',
+  'sunny': '',
+  'fair (night)': '',
+  'fair (day)': '',
+  'mixed rain and hail': '',
+  'hot': '',
+  'isolated thunderstorms': '',
+  'scattered thunderstorms': '',
+  'scattered showers': '',
+  'scattered snow showers': '',
+  'heavy snow': '',
+  'partly cloudy': '',
+  'thundershowers': '',
+  'snow showers': '',
+  'isolated thundershowers': ''
+}
