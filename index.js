a4d03bfc61b0dad7c947bc9d48b648fab16f2362
Hugo Holmqvist <hugo.holm@live.com>
2017-01-16 15:20:24 +0200

    Fix broken foodlist entrypoint (#23)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const translations = require('./translations')
 var EVENTS_FILE = 'events.json'
 var GROUPS_FILE = 'groups.json'
 
-const FOODLIST_URL = 'http://jallu.ga/unicafe/food'
+const FOODLIST_URL = 'http://62.78.194.51/unicafe/food'
 const WEATHER_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22helsinki%22)%20and%20u=%27c%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 
 moment.locale('fi')
